274f935700c202716928be7bc373601e304cf17c
ANA BEATRIZ <oibibih@gmail.com>
2026-06-09 17:22:17 -0300

    atividade if else

---
+++ ATIVIDADES/Desafio/js/script.js
@@ -0,0 +1,15 @@
+//Nível Baixo
+/*1*/let nome = console.log("ana beatriz");
+
+/*2*/const pi = Number(console.log(3.14159));
+
+/*3*/let idade = 24, altura = 1.62, cidade = ("blumenau");
+console.log(`sua idade é ${idade} anos, sua altura é ${altura}cm e a cidade é ${cidade}`);
+
+/*4*/
+
+/*5*/ 
+
+//Nível Médio
+
+/*6*/
---
+++ ATIVIDADES/if e else atividade/script/script.js
@@ -0,0 +1,20 @@
+/*Lista de Exercícios - Estruturas Condicionais
+
+let numero1 = Number(prompt("digite um numero"))
+let numero2 = Number(prompt("digite outro numero"))
+if(numero1 >= numero2){
+    alert(numero1)
+}else{
+    alert(numero2)
+}*/
+
+/*2. Número positivo ou negativo
+let valor = Number(prompt("digite um valor positivo ou negativo "))
+if(valor >=0){
+    alert("positivo")
+}else{
+    alert("negativo")
+}*/
+
+/* 3. Identificação de sexo*/
+let ()
