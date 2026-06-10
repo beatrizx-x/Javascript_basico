fe25490c8c1be3227633228229ba8fae013fe721
ANA BEATRIZ <oibibih@gmail.com>
2026-06-10 15:41:57 -0300

    atividade if e else ate a questão 5

--- ATIVIDADES/if e else atividade/script/script.js
+++ ATIVIDADES/if e else atividade/script/script.js
@@ -16,5 +16,41 @@ if(valor >=0){
     alert("negativo")
 }*/
 
-/* 3. Identificação de sexo*/
-let ()
+/* 3. Identificação de sexo
+let sexo = prompt("digite F para feminino ou M para masculino").toLocaleUpperCase()
+const sexoF = "F"
+const sexoM = "M"
+if(sexo === sexoF){
+    alert("Feminino")
+} else if(sexo === sexoM){
+    alert("masculino")
+}else{
+    alert("invalido")
+}*/
+
+
+/* 4. Vogal ou consoante
+const vogalA = "a"
+const vogalE = "e"
+const vogalI = "i"
+const vogalO = "o"
+const vogalU = "u"
+let vogalConsoante = prompt("digite uma letra do alfabeto para saber se é vogal ou consoante")
+if(vogalConsoante === vogalA || vogalConsoante === vogalE || vogalConsoante === vogalI || vogalConsoante === vogalO ||  vogalConsoante === vogalU){
+    alert("vogal")
+}else{
+    alert("consoante")
+}
+*/
+
+/*## 5. Média de notas*/
+let nota1 = Number(prompt("digite sua primeira nota"))
+let nota2 = Number(prompt("digite sua segunda nota"))
+let soma = (nota1 + nota2) / 2 
+if(soma === 10){
+    alert("Aprovado com Distinção")
+} else if(soma >=7 && soma <= 9.99){
+    alert("Aprovado")
+}else{
+    alert("reprovado")
+}
